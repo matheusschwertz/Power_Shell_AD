e9d86c9365f1817889a06c98784eabeebfd448fe
matheusschwertz <matheusschwertz@gmail.com>
2024-03-13 20:47:13 -0300

    feature: Adding new attribute in extensionAttibute1

---
+++ ReplaceAttribute.ps1
@@ -0,0 +1,21 @@
+Import-Module ActiveDirectory
+
+# Ler nomes dos grupos do arquivo CSV
+$grupos = Import-Csv -Path "C:\Users\a.mschwertz\Documents\PowerShell\NomeGrupo.csv" | Select-Object -ExpandProperty NomeGrupo
+
+foreach ($grupo in $grupos) {
+    $group = Get-ADGroup -Identity $grupo -ErrorAction SilentlyContinue
+
+    if ($group) {
+        Set-ADGroup -Identity $group.DistinguishedName -Replace @{extensionAttribute1 = "NoSync"}
+
+        $group = Get-ADGroup -Identity $grupo -Properties extensionAttribute1
+        if ($group.extensionAttribute1 -eq "NoSync") {
+            Write-Output "O atributo extensionAttribute1 do grupo $grupo foi alterado com sucesso para 'NoSync'."
+        } else {
+            Write-Output "Falha ao alterar o atributo extensionAttribute1 do grupo $grupo."
+        }
+    } else {
+        Write-Output "Grupo $grupo não encontrado no Active Directory."
+    }
+}
